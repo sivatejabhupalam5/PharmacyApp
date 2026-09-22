69e84cf22e85df0c328ded17c9935b2c94a32250
tejasree963 <gadamsettytejasree@gmail.com>
2026-09-22 13:38:37 +0530

    Update event logs, medicines, and sales data; modify Program.cs for OpenAPI configuration

--- Pharmacyapp/PharmacyApp/Program.cs
+++ Pharmacyapp/PharmacyApp/Program.cs
@@ -3,16 +3,11 @@ using PharmacyApp.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
 builder.Services.AddSingleton<JsonDataStore>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
